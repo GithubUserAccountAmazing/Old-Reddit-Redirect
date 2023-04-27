e62b4868833d9d42b4c647e1fcaa472d33653b18
Rich K. MLS <105183376+originates@users.noreply.github.com>
2023-04-26 19:17:57 -0500

    Create oldRedditRedirect.js

---
+++ oldRedditRedirect.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name Old Reddit Redirect
+// @version      0.1
+// @description  redirects you to old.reddit.com
+// @author       github.com/originates
+// @match *://*.reddit.com/*
+// @run-at document-start
+// @grant        none
+// ==/UserScript==
+
+// Enforce strict mode for better code quality
+'use strict';
+
+// Declare constant for current URL
+const currentUrl = window.location.href;
+
+// Declare constant for old reddit URL
+const oldRedditUrl = 'https://old.reddit.com/';
+
+// Check if the current URL does not include old.reddit.com
+if (!currentUrl.includes("old.reddit.com")) {
+
+  // Use regex literal and constant for new URL
+  const newUrl = currentUrl.replace(/^https?:\/\/(www\.)?reddit.com\//, oldRedditUrl);
+
+  // Redirect to new URL without history entry
+  window.location.replace(newUrl);
+}
